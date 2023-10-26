73c467217724f45ce2e33122a78550c3fbb3e8f5
_polmoz <mmaslowski@hotmail.com>
2023-10-26 01:55:08 -0400

    updates for python3

--- loopback.py
+++ loopback.py
@@ -8,11 +8,11 @@ import random
 
 def loopback_gen():
 
-    i = range(int(raw_input("How many interfaces would you like to generate? >>> ")))
+    i = range(int(input("How many interfaces would you like to generate? >>> ")))
 
-    j = raw_input("Which class subnet mask should we use? -options: a, b, c, all >>> ")
+    j = input("Which class subnet mask should we use? -options: a, b, c, all >>> ")
     while j not in ['a', 'b', 'c', 'all']:
-        j = raw_input("Which class subnet mask should we use? -options: a, b, c, all >>> ")
+        j = input("Which class subnet mask should we use? -options: a, b, c, all >>> ")
 
     masks_A = ('128.0.0.0', '192.0.0.0', '224.0.0.0', '240.0.0.0', '248.0.0.0', '252.0.0.0', '254.0.0.0', '255.0.0.0')
     masks_B = ('255.128.0.0', '255.192.0.0', '255.224.0.0', '255.240.0.0', '255.248.0.0', '255.252.0.0', '255.254.0.0', '255.255.0.0')
@@ -28,14 +28,14 @@ def loopback_gen():
     elif j == 'all':
           masks = masks_A + masks_B + masks_C + masks_D
     else:
-        print "Improper option provided"
+        print ("Improper option provided")
 
 
 
     loopback_num = 10000
 
-    print "\n\n"
-    print "%20s %20s %20s %20s %20s %20s %20s" % ("IP_Address", "Subnet_Mask", "First_Octet_Binary", "Second_Octet_Binary", "Third_Octet_Binary", "Fourth_Octet_Binary",  "Bit_Count",)
+    print ("\n\n")
+    print ("%20s %20s %20s %20s %20s %20s %20s" % ("IP_Address", "Subnet_Mask", "First_Octet_Binary", "Second_Octet_Binary", "Third_Octet_Binary", "Fourth_Octet_Binary",  "Bit_Count",))
 
     for number in i:
           a = (random.randrange(1,223))
@@ -57,9 +57,9 @@ def loopback_gen():
           y3 = third_octet_bin
           y4 = fourth_octet_bin
 
-          print "\n"
-          print "interface loopback"+(str(loopback_num+number))
-          print "ip address " + str(a) + "." + str(b) + "." + str(c) + "." + str(d) + " " + x
+          print ("\n")
+          print ("interface loopback"+(str(loopback_num+number)))
+          print ("ip address " + str(a) + "." + str(b) + "." + str(c) + "." + str(d) + " " + x)
 
 if __name__ == '__main__':
     loopback_gen()
